ab9a5e3006e9e5189a428045f69af1cd6b2e5ebf
Bradley J. Clement <bclement@jpl.nasa.gov>
2019-01-24 14:21:35 -0800

    script now cleans up failed mms java client build

--- setupMmsJavaClient.sh
+++ setupMmsJavaClient.sh
@@ -1,9 +1,13 @@
-if [ -e ../mms-java-client ]; then
+#!/bin/bash
+if [ -e `ls -1 ../mms-java-client/target/swagger*.jar | tail -n 1` ]; then
     echo "Already set up."
 else
     zipFile=`pwd`"/"`ls -1 mms*.zip`
     echo $zipFile
     unzip "$zipFile"
+    if [ -e ../mms-java-client ]; then
+        rm -rf ../mms-java-client
+    fi
     mv mms ../mms-java-client
     pushd ../mms-java-client
     mvn clean package
